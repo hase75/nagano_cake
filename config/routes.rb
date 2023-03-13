906360e45a14ef97686379df84db459d06cde4d2
hase75 <122374696+hase75@users.noreply.github.com>
2023-03-13 07:16:01 +0000

    [Add]ジャンル一覧 途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    root to: 'homes#top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
