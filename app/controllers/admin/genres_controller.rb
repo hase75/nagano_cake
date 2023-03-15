568ef3526d2f3508f3b91df626ccb2c7a0e3aaec
hase75 <122374696+hase75@users.noreply.github.com>
2023-03-15 06:20:23 +0000

    [Add]ジャンル一覧　途中

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,22 +1,22 @@
 class Admin::GenresController < ApplicationController
-  
+
   def index
     @genre = Genre.new
   end
-  
+
   def create
-    @genre = Genre.new(genre_params)
+    @genre = Genre.new(genres_params)
     @genre.save
     redirect_to admin_genres_path
   end
-  
-  
-  
-  
+
+
+
+
   private
 
-  def genre_params
-    params.require(:genre).permit(:name)
+  def genres_params
+    params.require(:genres).permit(:name)
   end
-  
+
 end
