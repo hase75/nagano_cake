687e02dc62cc2a05107e78c5c3e22fb851a96eaf
hase75 <122374696+hase75@users.noreply.github.com>
2023-03-16 05:52:35 +0000

    [Add]商品新規登録画面 ビューに記述

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,9 +3,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_items_path
   end
 
   def show
@@ -16,4 +20,12 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
+  
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :image)
+  end
+  
 end
