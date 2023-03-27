cf49fa2827a73aa3a11b81e735d6a5bd2ecea246
hase75 <122374696+hase75@users.noreply.github.com>
2023-03-27 12:51:25 +0000

    [Update]配送先登録・一覧画面と配送先編集画面の更新

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -7,6 +7,7 @@ class Public::AddressesController < ApplicationController
 
   def create
     address = Address.new(address_params)
+    address.customer_id = current_customer.id
     address.save
     redirect_to addresses_path
   end
@@ -18,6 +19,12 @@ class Public::AddressesController < ApplicationController
   def update
     address = Address.find(params[:id])
     address.update(address_params)
+    redirect_to addresses_path
+  end
+
+  def destroy
+    address = Address.find(params[:id])
+    address.destroy
     redirect_to addresses_path
   end
 
